11018d87925703b4e789a8124daddb0f64d6296b
Sapphiren <143931659+TheSapphRyz@users.noreply.github.com>
2024-05-16 15:19:00 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,1 @@
-// static/script.js
-document.getElementById('button-form').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent form submission
 
-    var formData = new FormData(event.target);
-
-    fetch(event.target.action, {
-        method: 'POST',
-        body: formData
-    })
-    .then(response => response.text())
-    .then(data => {
-        var responseContainer = document.getElementById('response-container');
-        responseContainer.textContent = data;
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
-});
